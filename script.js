786c2708c3a60b4c844e199f7f001cba59851c68
Bruno <brunooshiro27@gmail.com>
2022-05-13 17:50:28 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+
+var inadults = document.getElementById("adult")
+var inchild = document.getElementById("child")
+var induration = document.getElementById("duration")
+var result = document.getElementById("result")
+
+
+
+
+function calculo(){
+  let adults = inadults.value
+  let childs =  inchild.value
+  var durations=  induration.value
+
+
+ let carne = carnepp(durations) * adults + carnepp(durations) * childs / 2
+  let cerveja = cervejapp(durations) * adults
+  let bebidas = bebidaspp(durations) * adults + bebidaspp(durations) * childs / 2
+  
+
+  result.innerHTML = `<p>Serão necessários ${carne / 1000} Kg de carne</p>`
+   result.innerHTML += `<p>Serão necessários ${cerveja / 1000} L de cerveja</p>`
+  result.innerHTML += `<p>Serão necessários ${bebidas / 1000} L de bebidas</p>`
+
+}
+function carnepp(durations){
+  if(durations <= 6){
+    return 400
+  } else{
+    return 600
+  }
+
+}
+function cervejapp (durations){
+  if(durations <= 6){
+    return 1200
+  } else{
+    return 2000
+  }
+}
+function bebidaspp (durations){
+  if( durations <= 6){
+    return 1200
+  } else{
+    return 2000
+  }
+}
